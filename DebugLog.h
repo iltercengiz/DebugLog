9614e5c42f6977c4e9399bf6b2675247789fdebc
Ilter Cengiz <me@iltercengiz.info>
2014-09-20 11:30:40 +0300

    Added DebugLog.h

---
+++ DebugLog.h
@@ -0,0 +1,26 @@
+//
+// DebugLog.h
+//
+// Created by Ilter Cengiz on 20/09/2014.
+// Copyright (c) 2014 Ilter Cengiz. All rights reserved.
+//
+
+#ifndef DebugLog_h
+#define DebugLog_h
+
+#if DEBUG
+
+#define DebugLog(...) \
+    do{\
+        printf("[%s:%d]", __FUNCTION__, __LINE__);\
+        NSString *_S_ =  [NSString stringWithFormat:__VA_ARGS__];\
+        printf(" %s\n",[_S_ cStringUsingEncoding:NSUTF8StringEncoding]);\
+    } while(0);
+
+#else
+
+#define DebugLog(...)
+
+#endif
+
+#endif
